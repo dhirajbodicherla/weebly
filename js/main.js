64c3a0bca06bb89ad12fb1424027b7a0a6abe302
Dhiraj Bodicherla <dhiraj.bodicherla@gmail.com>
2014-12-24 23:43:38 -0500

    working on editor. parallel: todo in reactjs

--- js/main.js
+++ js/main.js
@@ -1,3 +1,25 @@
 $('.checkbox').click(function(){
 	$(this).toggleClass('checked');
 });
+
+// $('.image').draggable({
+// 	helper: "clone",
+// 	revert: function(dropped) {
+//      var dropped = dropped && dropped[0].id == "droppable";
+//      return !dropped;
+//   }
+// });
+
+// $(".editor").droppable({
+//     accept: ".image",
+//     drop: function (event, ui) {
+//         $(this).append($(ui.draggable).clone());
+//     },
+//     over: function (event, elem) {
+//         $(this).addClass("over");
+//         console.log("over");
+//     },
+//     out: function (event, elem) {
+//         $(this).removeClass("over");
+//     }
+// });
