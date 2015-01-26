0b8e19ebac34bf6282d1d3c0330669d26f67d1e0
Dhiraj Bodicherla <dhiraj.bodicherla@gmail.com>
2015-01-26 18:35:29 -0500

    compiled css and added heroku env

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -24,11 +24,11 @@ $app = new Illuminate\Foundation\Application;
 |
 */
 
-$env = $app->detectEnvironment(array(
+$env = $app->detectEnvironment(function(){
 
 	return $_SERVER['MY_LARAVEL_ENV'];
 
-));
+});
 
 /*
 'local' => array('DhirajBodicherla'),
